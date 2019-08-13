4eabdcd15bd43db4d47bc9b678fc990bce57845d
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-13 18:51:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,23 +3,15 @@ def unsafe?(speed)
     say = true 
     return say
   end
-end
-
-def unsafe?(speed)
   if speed < 40 
     say = true 
     return say
+  end
   if speed>40 && speed<60
     say = false
     return say
   end
-  end
 end
-
-def unsafe?(speed)
-  
-end
-
 
 def not_safe?(speed)
 	
